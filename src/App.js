34f1ba38019296cfa5183fa7d22cb89fb43bffe3
Juan Bautista María Beck <juanbautistabeck@gmail.com>
2024-11-24 00:08:04 -0300

    new color buttons

--- src/App.js
+++ src/App.js
@@ -119,9 +119,9 @@ function App() {
                   </>
                 ) : (
                   <Link to="/login">
-                    <Button variant="default" className="gap-2">
+                    <Button variant="outline" className="w-full gap-2 text-sm hover:bg-[#574a90] dark:hover:bg-[#574a90] hover:text-white transition-colors">
                       <svg 
-                        className="h-5 w-5" 
+                        className="h-5 w-5 " 
                         viewBox="0 0 24 24"
                         xmlns="http://www.w3.org/2000/svg"
                       >
